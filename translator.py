26a54106998792fdd8a3d74afac836cd92e494e3
sigrunahlquist <sigrun.ahlquist@gmail.com>
2017-03-17 10:24:50 -0400

    Update translator.py
    added do loop support

--- translator.py
+++ translator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+#function to print the tabs at the beginning of the lines
 def printtabs(f, i):
 	if i < 1:
 		return
@@ -8,14 +9,17 @@ def printtabs(f, i):
 			fo.write("\t")
 	return
 
+#variables; fi should probably be set as a command line argument
 index = 0;
 fi = open("program.f90", r)
 fo = open("program.py", w)
 
-
+#getting the line and splitting it into tokens
+codeline = fi.getline()
 tokens = codeline.split(" ")
 
-if tokens[0] == "IF":
+#if statement controls
+if (tokens[0] == "IF") or (tokens[0] == "if"):
 	printtabs(f, index)
 	fo.write("if")
 	counter = 1
@@ -24,7 +28,7 @@ if tokens[0] == "IF":
 		counter = counter + 1
 	fo.write(":\n")
 	index = index + 1
-if tokens[0] == "ELSE":
+if (tokens[0] == "ELSE") or (tokens[0] == "else"):
 	index = index - 1
 	printtabs(f, index)
 	fo.write("elif")
@@ -34,7 +38,21 @@ if tokens[0] == "ELSE":
 		counter = counter + 1
 	fo.write(":\n")
 	index = index + 1
-if tokens[0] == "ENDIF":
+if (tokens[0] == "ENDIF") or (tokens[0] == "endif"):
 	index = index - 1
 	printtabs(f, index)
 	fo.write(":\n")
+#do loop controls
+if (tokens[0] == "DO") or (tokens[0] == "do"):
+	printtabs(f, index)
+	fo.write("for ", tokens[2], " in range(", tokens[4], tokens[5], ":")
+	index = index + 1
+if (tokens[0] == "ENDDO") or (tokens[0] == "enddo"):
+	index = index - 1
+	printtabs(f, index)
+	fo.write("\n")
+if (tokens[0] == "END") or (tokens[0] == "end"):
+	#some loops use "end for" or "end do" rather than run together
+	index = index - 1
+	printtabs(f, index)
+	fo.write("\n")	
